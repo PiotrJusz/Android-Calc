3cffde4eb87435becac7f67c394204fa5ce98237
PiotrJusz <94100033+PiotrJusz@users.noreply.github.com>
2022-03-04 06:03:36 +0000

    1. Ustawiono tekst w polu editTextNumberDecimal na wartośc textEnd. 2. Dodano obsługe przycisku +/-; 3. Obsługa przycisku C.

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //Button button_plus_minus = (Button)findViewById(R.id.button_plus_minus);
+
+
+
+    public void addNumber(View view){
+        Log.i("Zdarzenie:","Wciśnięto przycisk!");
+        Log.i("Button", String.valueOf(view.getId()));
+
+        EditText editTextNumberDecimal = (EditText) findViewById(R.id.editTextNumberDecimal);
+        String screenResult;
+        Float floatScreenResult;
+
+        screenResult = String.valueOf(editTextNumberDecimal.getText());
+        floatScreenResult = Float.valueOf(screenResult);
+
+        if (view.getId()==R.id.button_plus_minus){
+            Log.i("Informacja:","Wciśnięto przycisk plus/minus");
+            floatScreenResult = (-1) * floatScreenResult;
+        }
+        else if(view.getId()==R.id.button_C){
+            Log.i("Wciśnięto przycisk:","przucisk C.");
+            floatScreenResult = Float.valueOf(0);
+        }
+
+        screenResult = String.valueOf(floatScreenResult);
+        editTextNumberDecimal.setText(screenResult);
+    }
+
+
 }
